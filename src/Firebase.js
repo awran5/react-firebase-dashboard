79d5448d0ffffdb501b575dea8999af9638ddf1c
Ahmed Khalil <awran5@yahoo.com>
2020-10-07 08:20:56 +0200

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,14 +4,7 @@ import "firebase/firestore"
 import "firebase/storage"
 
 export const App = firebase.initializeApp({
-  apiKey: "AIzaSyDSNuRV4OxfZJR19UvyG8WFrR5GyuFA2Z4",
-  authDomain: "firechat-da1c0.firebaseapp.com",
-  databaseURL: "https://firechat-da1c0.firebaseio.com",
-  projectId: "firechat-da1c0",
-  storageBucket: "firechat-da1c0.appspot.com",
-  messagingSenderId: "12211814375",
-  appId: "1:12211814375:web:c018c774eeb69f6e5f8742",
-  measurementId: "G-YXLH6NRVH4",
+  // Firebase Config
 })
 
 export const Auth = firebase.auth()
